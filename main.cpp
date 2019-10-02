ae604674cb0318eca465c6ac80aac857a86f53b7
stirlitzi <33487154+stirlitzi@users.noreply.github.com>
2019-10-02 10:27:23 +0300

    Update main.cpp
    Fix bugs with "{"

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-#include <climits>
 #include <iostream>
 #include <deque>
 #include <fstream>
 #include <chrono>
 #include <string>
+#include "conio.h"
 
 using namespace std;
 
+
+
+
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Usage: bulk N.";
-        return 0;
-    } else {
+     if (argc != 2) {
+         std::cout << "Usage: bulk N.";
+         return 0;
+     } else {
         int N;
         char *p;
         long conv = strtol(argv[1], &p, 10);
@@ -19,103 +23,115 @@ int main(int argc, char **argv) {
         } else {
             N = conv;
         }
-        chrono::system_clock::time_point time;
-//        int N = 5;
-        string cmd;
-        deque<string> cmd_data;
-        while (cin) {
-            for (size_t i = 0; i < N; i++) {
-                getline(cin, cmd);
-                if (cmd.empty()) {
+    chrono::system_clock::time_point time;
+    string cmd;
+    deque<string> cmd_data;
+    while (cin) {
+        for (size_t i = 0; i < N; i++) {
+                getline(cin,cmd);
+            if (cmd.empty()) {
+                break;
+            }
+            if (cmd == "{") {
+                if (!cmd_data.empty()) {
+                    time = chrono::system_clock::now();
+                    std::ofstream outfile("bulk" + to_string(
+                            chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
+                                          ".log");
+                    cout << "bulk: ";
+                    outfile << "bulk: ";
+                    bool not_first = false;
+                    for (auto &cmd_item:cmd_data) {
+                        if (not_first) {
+                            cout << ", ";
+                            outfile << ", ";
+                        }
+                        cout << cmd_item;
+                        outfile << cmd_item;
+                        not_first = true;
+                    }
+                    cout << endl;
+                    outfile << endl;
+                    outfile.close();
+                    cmd_data.clear();
+                }
+                string temp_cmd;
+                while (temp_cmd != "}") {
+                    cin >> temp_cmd;
+                    size_t counter = 0;
+                    size_t counter_cmd = 0;
+                    if (temp_cmd == "{") {
+                        ++counter_cmd;
+                    } else if (temp_cmd == "}") {
+                        ++counter;
+                    }
+                    if (temp_cmd == "{") {
+                        do {
+//                            while (temp_cmd != "}") {
+                            cin >> temp_cmd;
+                            if (temp_cmd == "{") {
+                                ++counter_cmd;
+                            } else if (temp_cmd == "}") {
+                                ++counter;
+                            } else {
+//                                if (temp_cmd != "}" && temp_cmd != "{") {
+                                cmd_data.push_back(temp_cmd);
+                            }
+                        } while (counter_cmd != counter);
+                        temp_cmd = "";
+                    } else if (temp_cmd != "}") {
+                        cmd_data.push_back(temp_cmd);
+                    }
+                }
+                if (!cmd_data.empty()) {
+                    time = chrono::system_clock::now();
+                    std::ofstream outfile("bulk" + to_string(
+                            chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
+                                          ".log");
+                    bool not_first = false;
+                    cout << "bulk: ";
+                    outfile << "bulk: ";
+                    for (auto &cmd_item:cmd_data) {
+                        if (not_first) {
+                            cout << ", ";
+                            outfile << ", ";
+                        }
+                        cout << cmd_item;
+                        outfile << cmd_item;
+                        not_first = true;
+                    }
+                    cout << endl;
+                    outfile << endl;
+                    outfile.close();
+                    cmd_data.clear();
                     break;
                 }
-                if (cmd == "{") {
-                    if (!cmd_data.empty()) {
-                        time = chrono::system_clock::now();
-                        std::ofstream outfile("bulk" + to_string(
-                                chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
-                                              ".log");
-                        cout << "bulk: ";
-                        outfile << "bulk: ";
-                        bool not_first = false;
-                        for (auto &cmd_item:cmd_data) {
-                            if (not_first) {
-                                cout << ", ";
-                                outfile << ", ";
-                            }
-                            cout << cmd_item;
-                            outfile << cmd_item;
-                            not_first = true;
-                        }
-                        cout << endl;
-                        outfile << endl;
-                        outfile.close();
-                        cmd_data.clear();
-                    }
-                    string temp_cmd;
-                    while (temp_cmd != "}") {
-                        cin >> temp_cmd;
-                        if (temp_cmd == "{") {
-                            while (temp_cmd != "}") {
-                                cin >> temp_cmd;
-                                if (temp_cmd != "}") {
-                                    cmd_data.push_back(temp_cmd);
-                                }
-                            }
-                            temp_cmd = "";
-                        } else if (temp_cmd != "}") {
-                            cmd_data.push_back(temp_cmd);
-                        }
-                    }
-                    if (!cmd_data.empty()) {
-                        time = chrono::system_clock::now();
-                        std::ofstream outfile("bulk" + to_string(
-                                chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
-                                              ".log");
-                        bool not_first = false;
-                        cout << "bulk: ";
-                        outfile << "bulk: ";
-                        for (auto &cmd_item:cmd_data) {
-                            if (not_first) {
-                                cout << ", ";
-                                outfile << ", ";
-                            }
-                            cout << cmd_item;
-                            outfile << cmd_item;
-                            not_first = true;
-                        }
-                        cout << endl;
-                        outfile << endl;
-                        outfile.close();
-                        cmd_data.clear();
-                        break;
-                    }
-                }
-                cmd_data.push_back(cmd);
             }
-            if (!cmd_data.empty()) {
-                time = chrono::system_clock::now();
-                std::ofstream outfile("bulk" + to_string(
-                        chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
-                                      ".log");
-                bool not_first = false;
-                cout << "bulk: ";
-                outfile << "bulk: ";
-                for (auto &cmd_item:cmd_data) {
-                    if (not_first) {
-                        cout << ", ";
-                        outfile << ", ";
-                    }
-                    cout << cmd_item;
-                    outfile << cmd_item;
-                    not_first = true;
-                }
-                cout << endl;
-                outfile << endl;
-                outfile.close();
-                cmd_data.clear();
-            }
+            cmd_data.push_back(cmd);
         }
-        return 0;
+        if (!cmd_data.empty()) {
+            time = chrono::system_clock::now();
+            std::ofstream outfile("bulk" + to_string(
+                    chrono::time_point_cast<std::chrono::milliseconds>(time).time_since_epoch().count()) +
+                                  ".log");
+            bool not_first = false;
+            cout << "bulk: ";
+            outfile << "bulk: ";
+            for (auto &cmd_item:cmd_data) {
+                if (not_first) {
+                    cout << ", ";
+                    outfile << ", ";
+                }
+                cout << cmd_item;
+                outfile << cmd_item;
+                not_first = true;
+            }
+            cout << endl;
+            outfile << endl;
+            outfile.close();
+            cmd_data.clear();
+        }
+    }
+    return 0;
     }
 }
